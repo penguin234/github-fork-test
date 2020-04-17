ca7fba2c449fa1e9e20f16649f7a4747c4c3c8f4
JaeHyun Hwang <hjh6195@naver.com>
2020-04-17 11:42:21 +0900

    added labels

--- gugudan.cpp
+++ gugudan.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
 	for (int i = 1; i < 9; i++)
+		cout << i << " Dan:" << endl;
 		for (int j = 1; j <= 9; j++)
 			cout << i << " * " << j << " = " << i * j << endl;
+		cout << endl;
 	return 0;
 }
